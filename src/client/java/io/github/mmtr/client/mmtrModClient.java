171c999e75b6f78e75f61e6f2a8fa63476a2f344
ieshishinjin <mncvtv@outlook.com>
2026-06-21 10:31:52 +0800

    git init

---
+++ src/client/java/io/github/mmtr/client/mmtrModClient.java
@@ -0,0 +1,10 @@
+package io.github.mmtr.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mmtrModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
